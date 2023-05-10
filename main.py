9c512211d2bd3ab882016fc13785d94ffda00892
TanyaDoronina <tmdoronina@gmai.com>
2023-05-10 14:14:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+price_all = 0
+while True:
+    try:
+        ticket_number = input('Какое число билетов вы хотите приобрести на конференцию? ')
+        ticket_number = int(ticket_number)
+        if type(ticket_number) == int:
+            break
+    except ValueError:
+        print('Введите целое число')
+for i in range(ticket_number):
+    i += 1
+    while True:
+        try:
+            age_for_ticket = input(f'Для какого возраста нужен билет №{i}? ')
+            age_for_ticket = int(age_for_ticket)
+            if age_for_ticket < 18:
+                print('Билет бесплатный')
+            elif 25 > age_for_ticket >= 18:
+                price_all += 990
+                print('Стоимость билета: 990 руб.')
+            else:
+                price_all += 1390
+                print('Стоимость билета: 1390 руб.')
+            if type(age_for_ticket) == int:
+                break
+        except ValueError:
+            print('Введите целое число')
+if ticket_number > 3:
+    price_all = price_all - ((price_all / 100) * 10)
+    print(f'Сумма к оплате {price_all} руб. с учетом 10%-ой скидки на полную стоимость заказа при регистрации на конфиренцию более 3 человек')
+else:
+    print(f'Сумма к оплате {price_all} руб.')
+
